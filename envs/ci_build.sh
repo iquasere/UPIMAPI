796ead816649a82db0b82c6454a1cd0f6dd74c92
iquasere <maildosequeira@gmail.com>
2022-06-29 16:12:24 +0100

    Put apt-get packages in build.sh

--- envs/ci_build.sh
+++ envs/ci_build.sh
@@ -2,3 +2,4 @@ PREFIX="/opt/conda"
 mkdir -p "${PREFIX}/bin"
 cp UPIMAPI/upimapi.py UPIMAPI/resources/* "${PREFIX}/bin"
 chmod +x /opt/conda/bin/upimapi.py
+apt-get install -y packagekit-gtk3-module libasound2 libdbus-glib-1-2 libx11-xcb1
